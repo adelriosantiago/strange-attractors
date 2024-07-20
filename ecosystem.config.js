a764c9beccc12108ee36576487c6cb264f0e13ca
Alejandro del Rio Santiago (@adelriosantiago) <adelriosantiago@gmail.com>
2024-07-20 14:32:25 -0400

    Add ecosystem and dev command, update Cloudflare tracking

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: "lorenz.adelriosantiago.com",
+      script: "./node_modules/@vue/cli-service/bin/vue-cli-service.js",
+      args: "start",
+    },
+  ],
+}
