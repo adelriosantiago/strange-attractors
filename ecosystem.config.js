d190127f1aaa7f4fd78b96147b6cd6640a92b94f
Alejandro del Rio Santiago (@adelriosantiago) <adelriosantiago@gmail.com>
2024-08-21 19:22:49 -0600

    Fix pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "lorenz.adelriosantiago.com",
-      script: "./node_modules/@vue/cli-service/bin/vue-cli-service.js",
-      args: "start",
+      script: "node",
+      args: "index.js",
     },
   ],
 }
